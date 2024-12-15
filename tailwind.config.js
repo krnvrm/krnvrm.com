52a022a0ce200a2d42b00d93070ebe4b37f77a7a
Karan Verma <83867201+krnvrm@users.noreply.github.com>
2024-12-15 14:44:36 +0530

    module rebuilt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-            "./index.html"],
+  content: ["./build/*.{html,js}"],
   theme: {
     extend: {},
   },
